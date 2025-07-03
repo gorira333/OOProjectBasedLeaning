43ace3f3b8f54f2b6d89d6751af04ce6da1db7c9
翼 吉田 <C3333@oic.jp>
2025-07-03 15:04:03 +0900

    社員ID確認

--- OOProjectBasedLeaning/CompanyForm.cs
+++ OOProjectBasedLeaning/CompanyForm.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Windows.Forms;
 using OOProjectBasedLeaning;
 
+
 namespace OOProjectBasedLeaning
 {
     /*
@@ -14,6 +15,8 @@ namespace OOProjectBasedLeaning
     {
         private Company company = NullCompany.Instance;
         private FlowLayoutPanel workPanelArea;
+        public Company CompanyInstance => company;
+
 
         public CompanyForm()
         {
--- OOProjectBasedLeaning/Form1.cs
+++ OOProjectBasedLeaning/Form1.cs
@@ -1,28 +1,90 @@
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace OOProjectBasedLeaning
 {
-
     public partial class Form1 : Form
     {
-        /*
-        アプリケーションの起動時に呼ばれる「最初のフォーム」
-        */
+        private TextBox idInputBox;
+        private Button searchButton;
+        private Label resultLabel;
 
         public Form1()
         {
-
             InitializeComponent();
 
-            // 従業員の作成
+            // 元の機能はそのまま
             new EmployeeCreatorForm().Show();
-
-            // 家
             new HomeForm().Show();
-
-            // 会社
             new CompanyForm().Show();
 
+            // UI初期化を追加
+            InitializeSearchUI();
         }
 
-    }
+        private void InitializeSearchUI()
+        {
+            this.Text = "メインフォーム（検索機能付き）";
+            this.Size = new Size(400, 200);
+            this.Font = new Font("Segoe UI", 10);
 
+            idInputBox = new TextBox
+            {
+                Location = new Point(20, 20),
+                Width = 200,
+                PlaceholderText = "社員IDを入力"
+            };
+
+            searchButton = new Button
+            {
+                Text = "検索",
+                Location = new Point(230, 18),
+                Size = new Size(80, 30),
+                BackColor = Color.DodgerBlue,
+                ForeColor = Color.White
+            };
+            searchButton.Click += SearchButton_Click;
+
+            resultLabel = new Label
+            {
+                Location = new Point(20, 60),
+                AutoSize = true
+            };
+
+            Controls.Add(idInputBox);
+            Controls.Add(searchButton);
+            Controls.Add(resultLabel);
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(idInputBox.Text, out int employeeId))
+            {
+                var companyForm = Application.OpenForms
+                    .OfType<CompanyForm>()
+                    .FirstOrDefault();
+
+                if (companyForm != null)
+                {
+                    var company = companyForm.CompanyInstance;
+                    var employee = company.FindEmployeeById(employeeId);
+
+                    if (employee is NullEmployee)
+                        resultLabel.Text = "社員が見つかりません";
+                    else
+                        resultLabel.Text = $"社員名：{employee.Name}";
+                }
+                else
+                {
+                    resultLabel.Text = "CompanyForm が開かれていません";
+                }
+            }
+            else
+            {
+                resultLabel.Text = "有効な社員IDを入力してください";
+            }
+        }
+    }
 }
