d0c7c6da0ef0932d045e818038d7ee9267755f15
翼 吉田 <C3333@oic.jp>
2025-07-10 14:17:15 +0900

    EmployeePanelのコメント追加と修正

--- OOProjectBasedLeaning/EmployeePanel.cs
+++ OOProjectBasedLeaning/EmployeePanel.cs
@@ -4,26 +4,32 @@ using System.Windows.Forms;
 
 namespace OOProjectBasedLeaning
 {
+    // 従業員情報を表示するパネル
     public class EmployeePanel : Panel
     {
+        // 従業員IDを外部から取得するためのプロパティ
         public int EmployeeId => employee.Id;
-        private Employee employee;
-        
+
+        // 従業員オブジェクトを外部に公開（必要な情報アクセス用）
         public Employee Employee => employee;
 
+        // 従業員データ
+        private Employee employee;
 
-       
-       
+
+        // コンストラクタ：パネルを初期化し、UIを構築する
+
         public EmployeePanel(Employee employee)
         {
             this.employee = employee;
 
             this.Size = new Size(320, 140);
             this.BorderStyle = BorderStyle.FixedSingle;
-
-            InitializeComponent();
-            SetupStatusButton();
+            // パネルのサイズと境界線を設定
+            InitializeComponent();      // ラベルとテキストボックスのUI構築
+            SetupStatusButton();        // 状態確認ボタンのUI構築
         }
+        // 名前表示ラベルと名前編集用テキストボックスを初期化
 
         private void InitializeComponent()
         {
@@ -33,14 +39,14 @@ namespace OOProjectBasedLeaning
                 AutoSize = true,
                 Location = new Point(20, 10)
             };
-
+            // 名前を編集するテキストボックス
             TextBox guestNameTextBox = new TextBox
             {
                 Text = employee.Name,
                 Location = new Point(140, 6),
                 Width = 160
             };
-            // ✅ TextBoxの変更処理
+            // テキスト変更時にラベルとEmployeeの名前を更新
             guestNameTextBox.TextChanged += (sender, e) =>
             {
                 employeeNameLabel.Text = guestNameTextBox.Text;
@@ -48,12 +54,13 @@ namespace OOProjectBasedLeaning
             };
 
 
+            // コントロールをパネルに追加
             Controls.Add(employeeNameLabel);
-                Controls.Add(guestNameTextBox);
+            Controls.Add(guestNameTextBox);
 
-            
+
         }
-
+        // 状態確認ボタンを追加し、勤務状態のダイアログ表示機能を実装
         public void SetupStatusButton()
         {
             var statusButton = new Button
@@ -62,7 +69,7 @@ namespace OOProjectBasedLeaning
                 Location = new Point(20, 50),
                 Size = new Size(110, 40)
             };
-
+            // ボタンクリック時に、社員情報と勤務状態を表示
             statusButton.Click += (sender, e) =>
             {
                 string message = $"社員ID：{employee.Id}\n" +
